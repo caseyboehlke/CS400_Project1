33993f6342969488ee0cab3f03cb475cd15815af
T. Eric. Doughery <32188051+Sl33per@users.noreply.github.com>
2020-09-27 17:56:06 -0500

    Add files via upload

--- LibraryTD.java
+++ LibraryTD.java
@@ -1,3 +1,4 @@
+
 // --== CS400 File Header Information ==--
 // Name: Timothy Eric Doughery
 // Email: doughery@wisc.edu
@@ -12,6 +13,15 @@ public class LibraryTD {
 	
 	public LibraryTD() {
 		foo = new HashTableMap<String, Book>();
+		Book a = new Book("Frankenstein", "Mary Shelley", "Fiction", 111, true);
+		Book b = new Book("King Lear", "William Shakespeare", "Drama", 222, true);
+		Book c = new Book("The Scarlet Letter", "Nathaniel Hawthorne", "Historical Fiction", 333, true);
+		Book d = new Book("Heart of Darkness", "Joseph Conrad", "Novel", 444, true);
+		Book e = new Book("To Kill a Mockingbird", "Harper Lee", "Fiction", 555, true);
+		Book[] bar = {a, b, c, d, e};
+		for (Book i: bar) {
+			foo.put(i.getTitle(), i);
+		}
 	}
 	
 	public boolean addBook(String title, String author, String genre, int isbn) {
@@ -19,8 +29,14 @@ public class LibraryTD {
 		return foo.put(title, temp);
 	}
 	
-	public Book retrieveBook(String key) { 
-		return foo.get(key);
+	public Book getBook(String key) { 
+		Book temp = null;
+		try {
+			temp = foo.get(key);
+		} catch (Exception e) {
+			
+		}
+		return temp;
 	}
 	
 	public int librarySize() {
@@ -34,18 +50,29 @@ public class LibraryTD {
 		return false;
 	}
 
-	public boolean checkoutBook(String key) {
-		if (foo.get(key).getCheckedIn()) { //if the book is available
-			foo.get(key).setCheckedIn(); //set it to unavailable
-			return true; 
+	public boolean checkOut(String key) {
+		try {
+			if (foo.get(key).getCheckedIn()) { //if the book is available
+				foo.get(key).setCheckedIn(); //set it to unavailable
+				return true; 
+			}
+		} catch (Exception e) {
+			
 		}
 		return false;
 	}
-	public boolean dropOffBook(String key) {
-		if (!foo.get(key).getCheckedIn()) { //I guess if the book is unavailable
-			foo.get(key).setCheckedIn(); //set it to available
-			return true;
+	public boolean checkIn(String key) {
+		try {
+			if (!foo.get(key).getCheckedIn()) { //I guess if the book is unavailable
+				foo.get(key).setCheckedIn(); //set it to available
+				return true;
+			}
+		} catch (Exception e) {
+			
 		}
 		return false;
+	}
+	public boolean isBookAvailable(String key) {
+		return foo.get(key).getCheckedIn();
 	}
 }

7b85f3ba0bd4cad0304e58c4ab021815a23d0186
Lindsay Dyjach <71901869+ldyjach@users.noreply.github.com>
2020-09-27 14:21:38 -0500

    Add files via upload

--- LibraryDB.java
+++ LibraryDB.java
@@ -10,49 +10,58 @@
 * This class serves as a database that holds Book objects. Books are provided by librarians
 * to add or remove and by students to search for the book in the database.
 */
-public class LibraryDB implements HashTableMap() {
-	
-	private HashTableMap<String, Book> db;
-	private Book[] firstBooks;
-	
-	public LibraryDB() {
-		db = new HashTableMap<String, Book>(20);
-		
-	}
-	
-	public boolean addBook(String title, String author, String genre, int ISBN){
-	// use put method to add to the library DB
-		Book book = new Book(title, author, genre, ISBN, true);
-		db.put(title, book);
-	}
-	
-	public boolean removeBook(String title){
+public class LibraryDB {
+  
+  private HashTableMap<String, Book> db;
+  private Book[] firstBooks;
+  
+  public LibraryDB() {
+    db = new HashTableMap<String, Book>(20);
+ 
+    
+  }
+  
+  public boolean addBook(String title, String author, String genre, int ISBN){
+  // use put method to add to the library DB
+    Book book = new Book(title, author, genre, ISBN, true);
+   return ( db.put(title, book)) ;
+    
+  }
+  
+  public boolean removeBook(String title){
+    int checkSize = db.size();
     // use remove method
-		db.remove(title);
-	}
-	
-	public String getBook(String title){
-    // 
-		db.get(title);
-	}
-	
+    db.remove(title);
+    if ((checkSize - db.size()) == 1) {
+      return true;
+    }
+    return false;
+  }
+  
+  public Book getBook(String title){
+    return db.get(title);
+  }
+  
 // return the concatenated string of all book variables
-	public int librarySize(){
-		return db.size;
-	}
-	
+  public int librarySize(){
+    return db.size();
+  }
+  
 //boolean method to see if the book is checked out or  not
-	public boolean isBookAvailable(String title){
-		return db.get(title).getCheckedIn();
-		
-	}
-	
-	public boolean checkOut(String title) {
-		if(db.isBookAvailable(title)) {
-			db.get(title).setCheckedIn(false);
-			return true
-		}
-		else return false;
-	}
-	
+  public boolean isBookAvailable(String title){
+    return db.get(title).getCheckedIn();
+    
+  }
+  
+  public boolean checkOut(String title) {
+    if(isBookAvailable(title)) {
+      db.get(title).setCheckedIn();
+      return true;
+    }
+    else return false;
+  }
+}
+  
+// change the book check out status
+  
 // change the book check out status
